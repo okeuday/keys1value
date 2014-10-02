82d0931d053991ce08508d898f9b9eb62c7c323b
Michael Truog <mjtruog@gmail.com>
2014-10-02 10:55:38 -0700

    Add hex.pm package.

---
+++ package.exs
@@ -0,0 +1,25 @@
+defmodule Keys1value.Mixfile do
+  use Mix.Project
+
+  def project do
+    [app: :keys1value,
+     version: "1.3.3",
+     description: description,
+     package: package,
+     deps: deps]
+  end
+
+  defp deps do
+  end
+
+  defp description do
+    "Erlang Set Associative Map For Key Lists"
+  end
+
+  defp package do
+    [files: ~w(src doc README.markdown),
+     contributors: ["Michael Truog"],
+     licenses: ["BSD"],
+     links: %{"GitHub" => "https://github.com/okeuday/keys1value"}]
+   end
+end
