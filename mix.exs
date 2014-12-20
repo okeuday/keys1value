8df9ab9851f3d3a0ff7e027528814f14285ebbcf
Michael Truog <mjtruog@gmail.com>
2014-12-20 14:08:19 -0800

    Version 1.4.0.

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Keys1value.Mixfile do
 
   def project do
     [app: :keys1value,
-     version: "1.4.0-rc.4",
+     version: "1.4.0",
      language: :erlang,
      description: description,
      package: package,
