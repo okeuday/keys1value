d12e143be560702a523070ee6e2aa610871cc2e2
Michael Truog <mjtruog@gmail.com>
2017-06-07 09:46:15 -0700

    Version 1.7.1.

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Keys1value.Mixfile do
 
   def project do
     [app: :keys1value,
-     version: "1.7.0",
+     version: "1.7.1",
      language: :erlang,
      description: description(),
      package: package(),
